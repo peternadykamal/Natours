3cf3d21719bbf367397f17e5c30399b057b273bc
peter nady <nady.peter347@gmail.com>
2024-02-28 15:53:24 +0200

    Starting Our API: Handling GET Requests

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "hello world from the express server", app: "Natours" });
-});
+const toursData = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, "utf-8")
+);
 
-app.post("/", (req, res) => {
-  res.end("You can post to this endpoint...");
+// route handler
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: toursData.length,
+    data: {
+      tours: toursData,
+    },
+  });
 });
 
 const port = 3000;
