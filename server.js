7e6b3d5892427d32157ea3c90b4457444daf521e
peter nady <nady.peter347@gmail.com>
2024-03-03 15:52:35 +0000

    Connecting Our Database with the Express App

--- server.js
+++ server.js
@@ -35,6 +35,23 @@ dotenv.config({ path: `${__dirname}/config.env` });
 
 const app = require("./app");
 
+// CONNECT WITH DATABASE
+let DB_USER = "";
+let DB_PASSWORD = "";
+let DB = "";
+if (process.env.NODE_ENV === "development") {
+  DB_USER = process.env.DATABASE_USERNAME;
+  DB_PASSWORD = process.env.DATABASE_PASSWORD;
+  DB = process.env.LOCAL_DB_CONNECTION_STRING;
+} else {
+  DB_USER = process.env.REMOTE_DATABASE_USERNAME;
+  DB_PASSWORD = process.env.REMOTE_DATABASE_PASSWORD;
+  DB = process.env.REMOTE_DB_CONNECTION_STRING;
+}
+
+DB = DB.replace("<user>", DB_USER).replace("<password>", DB_PASSWORD);
+mongoose.connect(DB).then(() => console.log("DB Connections successfully"));
+
 // START THE SERVER
 const port = process.env.PORt || 3000;
 app.listen(port, () => {
