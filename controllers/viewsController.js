44bef245e70dc0075aff802a87e5d2edbdbb98ab
peter nady <nady.peter347@gmail.com>
2024-03-31 13:58:35 +0000

    Building the Tour Overview

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,19 @@
-const getOverview = (req, res) => {
+const Tour = require("../models/tourModel");
+const { catchAsync } = require("../utils/catch");
+
+const getOverview = catchAsync(async (req, res) => {
+  // 1) get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) build template
+
+  // 3) render that template using tour data
+
   res.status(200).render("overview", {
     title: "All Tours",
+    tours: tours,
   });
-};
+});
 
 const getTour = (req, res) => {
   res.status(200).render("tour", {
