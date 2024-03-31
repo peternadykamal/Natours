a9c3c40a279a6c6969113daebc25f6495ffc0991
peter nady <nady.peter347@gmail.com>
2024-03-31 10:46:34 +0000

    Extending Our Base Template with Blocks

--- app.js
+++ app.js
@@ -84,6 +84,19 @@ app.get("/", (req, res) => {
     user: "Jonas",
   });
 });
+
+app.get("/overview", (req, res) => {
+  res.status(200).render("overview", {
+    title: "All Tours",
+  });
+});
+
+app.get("/tour", (req, res) => {
+  res.status(200).render("tour", {
+    title: "the forest hiker tour",
+  });
+});
+
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
