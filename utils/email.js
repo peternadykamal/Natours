02c43febde73c333712c9643b83e73588311f12c
peter nady <nady.peter347@gmail.com>
2024-04-03 09:31:39 +0000

    Building a Complex Email Handler

--- utils/email.js
+++ utils/email.js
@@ -1,4 +1,60 @@
 const nodeMailer = require("nodemailer");
+const pug = require("pug");
+const { convert } = require("html-to-text");
+
+class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(" ")[0];
+    this.url = url;
+    this.from = `Peter Nady ${process.env.EMAIL_FROM}`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === "production") {
+      // sendgrid
+      return 1;
+    }
+
+    return nodeMailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: 2525,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    // 1) render the html based on the pug template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        usl: this.url,
+        subject: subject,
+      }
+    );
+
+    // 2) define the email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject: subject,
+      html: html,
+      text: convert(html, { wordwrap: 130 }),
+    };
+
+    // 3) create a transport and send the email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send("welcome", "Welcome to the Natours Family!");
+  }
+}
 
 const sendEmail = async (options) => {
   // 1) create a transporter
@@ -14,7 +70,7 @@ const sendEmail = async (options) => {
 
   // 2) define the email options
   const mailOptions = {
-    from: "Peter Nady <hello@peter.io",
+    from: "Peter Nady <hello@peter.io>",
     to: options.email,
     subject: options.subject,
     text: options.message,
